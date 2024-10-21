7a355c2ccb8e5cb4cf703a2138d7e3dee8ef6eb1
Vaughn Kottler <vaughnkottler@gmail.com>
2024-10-20 19:55:21 -0500

    Add markdown sources

--- homestead/util.py
+++ homestead/util.py
@@ -12,11 +12,26 @@ import aiofiles
 
 # internal
 from runtimepy.net.arbiter.config import ConfigObject
+from runtimepy.net.server import RuntimepyServerConnection
 from vcorelib import DEFAULT_ENCODING
 
 ROOT = Path(sep)
 SYS = ROOT.joinpath("sys")
 PROC = ROOT.joinpath("proc")
+LIBRE_STR = "libre-embedded"
+LIBRE = ROOT.joinpath("opt", LIBRE_STR)
+
+
+def libre_repo_path(
+    repo: str, owner: str = LIBRE_STR, strict: bool = True
+) -> Path:
+    """Locate a source repository path."""
+
+    result = LIBRE.joinpath("src", owner, repo)
+    if strict:
+        assert result.is_dir(), result
+
+    return result
 
 
 def disable_ui_psutil(data: ConfigObject) -> None:
@@ -26,6 +41,15 @@ def disable_ui_psutil(data: ConfigObject) -> None:
         if struct["name"] == "ui":
             struct_cfg = struct.setdefault("config", {})
             struct_cfg["psutil"] = False
+
+
+def add_web_server_paths(_: ConfigObject) -> None:
+    """Add some disk paths to runtimepy's HTTP server."""
+
+    # Add markdown sources.
+    path = libre_repo_path(".github").joinpath("md")
+    if path not in RuntimepyServerConnection.class_paths:
+        RuntimepyServerConnection.class_paths.append(path)
 
 
 def read_str(path: Path) -> str:
